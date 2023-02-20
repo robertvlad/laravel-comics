f7353cd2e064423c26848cb2cf0674a2bffe69de
robertvlad <vladrob96@gmail.com>
2023-02-20 03:20:19 +0100

    Main aggiunto

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -2,12 +2,25 @@
     <div class="titan_bg">
 
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                @foreach ($cards as $card)
-                    
-                @endforeach
+    <div class="black-main">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div>
+                        <a class="current" href="#">CURRENT SERIES</a>
+                    </div>
+                    <div class="d-flex flex-wrap justify-content-between">
+                        @foreach (array_slice($cards, 0, 10) as $card)
+                            <div class="cards-comics">
+                                <img src="{{ $card['thumb'] }}" alt="">
+                                <p>{{ $card['title']}}</p>
+                            </div>                        
+                        @endforeach
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <a class="load" href="#">LOAD MORE</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
